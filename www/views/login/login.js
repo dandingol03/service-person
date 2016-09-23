36f8cc7219893f7b355be8f2faee9ca30c9864d1
EEvvee <dingyiming_sdu@163.com>
2016-09-23 15:04:13 +0800

    xx

--- www/views/login/login.js
+++ www/views/login/login.js
@@ -1,24 +1,15 @@
 /**
- * Created by yiming on 16/9/13.
- */
-/**
  * Created by danding on 16/9/6.
  */
 angular.module('starter')
 
-  .controller('loginController',function($scope,$state,$ionicLoading,$http
-    ,$ionicPopup,$timeout
+  .controller('loginController',function($scope,$state,$ionicLoading,$http,$ionicPopup,$timeout,$rootScope
     ,$cordovaFile,$cordovaFileTransfer,$ionicActionSheet,$cordovaCamera){
 
 
 
-    $scope.formUser = {};
 
-    var inputData = {
-      grant_type: 'password',
-      username: '123456789',
-      password: "1234"
-    };
+    $scope.formUser = {};
 
     $scope.user={};
 
@@ -72,7 +63,6 @@ angular.module('starter')
         }
         alert('error=' + error);
       });
-
     };
 
 
@@ -81,10 +71,11 @@ angular.module('starter')
 
       var access_token=null;
 
+
       $http({
         method:"POST",
         data:"grant_type=password&password=" + $scope.user.password + "&username=" + $scope.user.username,
-        url:"http://192.168.1.102:3000/login",
+        url:"/login",
         headers: {
           'Authorization': "Basic czZCaGRSa3F0MzpnWDFmQmF0M2JW",
           'Content-Type': 'application/x-www-form-urlencoded'
@@ -93,11 +84,64 @@ angular.module('starter')
       }).success(function(response){
 
         var access_token=response.access_token;
+        $state.go('register');
         if(access_token!==undefined&&access_token!==null)
         {
+          $rootScope.access_token=access_token;
+          if(window.cordova!=undefined && window.cordova!=null) {
+            var targetPath = cordova.file.externalRootDirectory + 'ionic.jpg';
 
-          var targetPath=cordova.file.externalRootDirectory+'ionic.jpg';
 
+            var carInfo = {
+              carNum: "5",
+              engineNum: "2",
+              frameNum: "3",
+              factoryNum: "4",
+              firstRegisterTime: "2016-01-01",
+              ownerName: "6",
+              ownerIdCard: "7",
+              ownerAddress: "8",
+              carPhoto: null,
+              ownerIdPhoto: null
+            };
+
+
+            $scope.photo = '';
+
+
+            //$cordovaFile.readAsBinaryString(cordova.file.externalRootDirectory, 'ionic.jpg')
+            //  .then(function (success) {
+            //    alert('content of image=' + success);
+            //    carInfo.carPhoto = success;
+            //    carInfo.ownerIdPhoto = success;
+            //    $http({
+            //      method: "POST",
+            //      url: "http://192.168.1.102:3000/svr/request",
+            //      headers: {
+            //        'Authorization': "Bearer " + access_token,
+            //      },
+            //      data: {
+            //        request: 'uploadCarAndOwnerInfo',
+            //        info: carInfo
+            //      }
+            //    }).
+            //      success(function (response) {
+            //        console.log('success');
+            //      }).
+            //      error(function (err) {
+            //        var str = '';
+            //        for (var field in err)
+            //          str += field + ':' + err[field];
+            //        console.log('error=' + str);
+            //      });
+            //  }, function (error) {
+            //    // error
+            //    var err = '';
+            //    for (var field in error)
+            //      err += field + ':' + error[field];
+            //    alert('error=' + err);
+            //  });
+          }
 
           var carInfo = {
             carNum : "5",
@@ -112,44 +156,50 @@ angular.module('starter')
             ownerIdPhoto: null
           };
 
+          $rootScope.access_token=access_token;
+          $state.go('tabs.dashboard');
+
 
           $scope.photo='';
 
 
+          /**
+           * $cordovaFile 读取文件
+           */
+          //$cordovaFile.readAsBinaryString(cordova.file.externalRootDirectory, 'ionic.jpg')
+          //  .then(function (success) {
+          //    alert('content of image=' + success);
+          //    carInfo.carPhoto=success;
+          //    carInfo.ownerIdPhoto=success;
+          //    $http({
+          //      method: "POST",
+          //      url: "http://192.168.1.102:3000/svr/request",
+          //      headers: {
+          //        'Authorization': "Bearer " + access_token,
+          //      },
+          //      data:
+          //      {
+          //        request:'uploadCarAndOwnerInfo',
+          //        info:carInfo
+          //      }
+          //    }).
+          //      success(function (response) {
+          //        console.log('success');
+          //      }).
+          //      error(function (err) {
+          //        var str='';
+          //        for(var field in err)
+          //          str+=field+':'+err[field];
+          //        console.log('error='+str);
+          //      });
+          //  }, function (error) {
+          //    // error
+          //    var err='';
+          //    for(var field in error)
+          //    err+=field+':'+error[field];
+          //    alert('error=' + err);
+          //  });
 
-          $cordovaFile.readAsBinaryString(cordova.file.externalRootDirectory, 'ionic.jpg')
-            .then(function (success) {
-              alert('content of image=' + success);
-              carInfo.carPhoto=success;
-              carInfo.ownerIdPhoto=success;
-              $http({
-                method: "POST",
-                url: "http://192.168.1.102:3000/svr/request",
-                headers: {
-                  'Authorization': "Bearer " + access_token,
-                },
-                data:
-                {
-                  request:'uploadCarAndOwnerInfo',
-                  info:carInfo
-                }
-              }).
-              success(function (response) {
-                console.log('success');
-              }).
-              error(function (err) {
-                var str='';
-                for(var field in err)
-                  str+=field+':'+err[field];
-                console.log('error='+str);
-              });
-            }, function (error) {
-              // error
-              var err='';
-              for(var field in error)
-                err+=field+':'+error[field];
-              alert('error=' + err);
-            });
 
 
 
@@ -186,27 +236,6 @@ angular.module('starter')
         });
     }
 
-    //文件下载
-    $scope.download=function(){
-      var url='http://192.168.0.199:9030/get/photo/home.jpg';
-      var targetPath=cordova.file.externalRootDirectory + "/home.jpg";
-      var trustHosts = true;
-      var options = {};
-      $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
-        .then(function(result) {
-          alert('success');
-        }, function(err) {
-          // Error
-          var str='';
-          for(var field in err)
-          {
-            str+=field+':'+err[field];
-          }
-          alert('error=====\r\n'+str);
-        }, function (progress) {
-
-        });
-    }
 
     //文件上传
     $scope.upload=function(){
@@ -228,7 +257,7 @@ angular.module('starter')
 
 
     $scope.test=function() {
-      $http.get("http://202.194.14.106:9030/insurance/get_lifeinsurance_list").
+      $http.get("http://202.194.14.106:3000/insurance/get_lifeinsurance_list").
       then(function(res) {
         if(res.data!==undefined&&res.data!==null)
         {
@@ -287,6 +316,37 @@ angular.module('starter')
       });
     }
 
+    $scope.uploadPhoto=function(){
+
+      $cordovaFileTransfer.upload(server, $scope.photo,options)
+        .then(function(result) {
+          var response=result.response;
+          var json=eval('('+response+')');
+          if(json.type!==undefined&&json.type!==null)
+          {
+            $ionicLoading.show({
+              template: json.content,
+              duration: 2000
+            });
+            //TODO:将本用户更新照片的消息通过websocket发送给其他用户
+
+          }else{
+            $ionicLoading.show({
+              template: "field type doesn't exist in response",
+              duration: 2000
+            });
+          }
+
+        }, function(err) {
+          // Error
+          alert("err:"+err);
+        }, function (progress) {
+          // constant progress updates
+        });
+
+    }
+
+
     $scope.uploadCarAndOwnerInfo=function()
     {
       $http.get("http://localhost:9030/insurance/get_lifeinsurance_list",
@@ -316,6 +376,20 @@ angular.module('starter')
       });
     }
 
+
+    $scope.baidu=function(){
+      $http({
+        method:'GET',
+        url:"/proxy/send",
+        headers:{
+          'Access-Control-Allow-Origin':'*'
+        }}).
+      success(function (response) {
+        console.log('success');
+      }).error(function(err) {
+        console.log('...');
+      });
+    }
 
 
   });
